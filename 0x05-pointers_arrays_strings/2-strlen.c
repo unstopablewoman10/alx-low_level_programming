7c102736b11926167cb2135a54e2a423544af1b2
unstopablewoman10 <hewitek@gmail.com>
2022-12-19 08:31:07 -0800

    very length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: input string
+ * Return: Length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' ; i++)
+		;
+	return (i);
+}
+
